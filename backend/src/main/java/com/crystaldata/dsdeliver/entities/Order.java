c5c9356790ffca125535b081681f907602141a9a
Henio Junior <henioajunior@gmail.com>
2021-01-05 18:24:08 -0300

    GET /orders

--- backend/src/main/java/com/crystaldata/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/crystaldata/dsdeliver/entities/Order.java
@@ -29,8 +29,7 @@ public class Order implements Serializable {
 	private Double longitude;
 	private Instant moment;
 	private OrderStatus status;
-	private Double total;
-	
+		
 	@ManyToMany
 	@JoinTable(name="tb_order_product",
 		joinColumns = @JoinColumn(name="order_id"),
@@ -41,14 +40,12 @@ public class Order implements Serializable {
 	
 	public Order(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status,
 			Double total) {
-		super();
 		this.id = id;
 		this.address = address;
 		this.latitude = latitude;
 		this.longitude = longitude;
 		this.moment = moment;
 		this.status = status;
-		this.total = total;
 	}
 	
 	public Long getId() {
@@ -97,14 +94,6 @@ public class Order implements Serializable {
 
 	public void setStatus(OrderStatus status) {
 		this.status = status;
-	}
-
-	public Double getTotal() {
-		return total;
-	}
-
-	public void setTotal(Double total) {
-		this.total = total;
 	}
 
 	public Set<Product> getProducts() {
